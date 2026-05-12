b6089a0a060a0276ffe8420eb22cd31a8ce5024d
Néo Carlier <neo.carlier2212@gmail.com>
2026-05-12 18:01:18 +0200

    fichier de création db

---
+++ backend/db/init-db.js
@@ -0,0 +1,13 @@
+const Database = require('better-sqlite3');
+const db = new Database('dev.db'); 
+
+// Création de la table User
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS User (
+    username TEXT NOT NULL PRIMARY KEY UNIQUE,
+    password TEXT NOT NULL
+  )
+`).run();
+
+console.log("Base de données et table User créées avec succès !");
+db.close();
